e684960a59f51093858b01984c266c16ae001b0b
Cristian Ogliari <cogliari@gmail.com>
2022-05-29 22:01:16 -0300

    first commit

---
+++ src/pages/home/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Redux implementation samples.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
